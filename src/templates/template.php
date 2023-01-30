a855ba391a0eb137ab0a9eb9ce317ed12c058f28
adamboissy2004@gmail.com <adamboissy2004@gmail.com>
2023-01-30 15:49:42 +0100

    css

--- src/templates/template.php
+++ src/templates/template.php
@@ -8,10 +8,7 @@
 	<link rel="stylesheet" href="assets/CSS/header.css">
   </head>
   <body>
-    
-	<div>
-		<?php require_once __DIR__ . '/partials/menu.php'; ?>
-	</div>  
+     
 	
 	<?= $page_content ;?>
 	<?= $page_scripts ;?>
