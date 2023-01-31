744bdf9591bab7ed05e07c8d63e708dcb296563c
FOUREST <jmfourest@gmail.com>
2023-01-31 12:07:42 +0100

    save

--- src/templates/pages/profile.php
+++ src/templates/pages/profile.php
@@ -14,25 +14,20 @@ ob_start();
 
     include_once __DIR__ . '/../../utils/alert_errors.php';
 
+    $usr_info = $db->prepare("SELECT * FROM users WHERE user_id=?");
+    $usr_info->execute([$_SESSION['user_id']]);
+    $info = $usr_info->fetch();
 ?>
+<div id="infos">
+    <div id="pfp">
+        <img src="actions/show_img.php" alt="profile picture" id="pic">
+        <!--<img src="assets/images/def.jpeg" alt="profile picture" id="pic">-->
+    </div>
 
-<div id="pfp">
-    <img src="actions/show_img.php" alt="profile picture" id="pic">
-    <!--<img src="assets/images/def.jpeg" alt="profile picture" id="pic">-->
+    <h2>Name: <?= $info['name']; ?></h2>
+    <h2>Username: <?= $info['pseudo']; ?></h2>
+    <h2>Email: <?= $info['email']; ?></h2>
 </div>
-
-<?php
-
-$usr_info = $db->prepare("SELECT * FROM users WHERE user_id=?");
-$usr_info->execute([$_SESSION['user_id']]);
-$info = $usr_info->fetch();
-
-?>
-
-<h2>Name: <?= $info['name']; ?></h2>
-<h2>Username: <?= $info['pseudo']; ?></h2>
-<h2>Email: <?= $info['email']; ?></h2>
-
 <?php
 
 $page_content = ob_get_clean();
