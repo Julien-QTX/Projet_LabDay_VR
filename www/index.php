119eda6806b89922eb70e7fffcc54c65d439ec86
BastienVitour <bvitour2004@gmail.com>
2023-01-31 10:17:41 +0100

    index

--- www/index.php
+++ www/index.php
@@ -1,13 +1,13 @@
 <?php
 require_once __DIR__ . "/../src/init.php";
 
-//$page = 'home';
+$page = 'home';
 
-//if (isset($_GET['page'])) {
-//    if (in_array($_GET['page'], $pages)) {
+if (isset($_GET['page'])) {
+    if (in_array($_GET['page'], $pages)) {
         $page = $_GET['page'];
-//    }
-//}
+    }
+}
 
 include_once __DIR__ . "/../src/templates/pages/$page.php";
 include_once __DIR__ . "/../src/templates/template.php";
