ef944e53de8a8667a41e1a92dbb3c3d382e67b87
FOUREST <jmfourest@gmail.com>
2023-04-05 11:49:30 +0200

    better ui

--- src/templates/pages/chatGlobal.php
+++ src/templates/pages/chatGlobal.php
@@ -6,17 +6,29 @@ if (!isset($_SESSION['user_id'])) {
     header('Location: /?page=login');
 }
 
-$page_title = "Appel";
+$usr_info = $db->prepare("SELECT pseudo FROM users WHERE user_id=?");
+$usr_info->execute([$_SESSION['user_id']]);
+$info = $usr_info->fetch();
+
+$page_title = "Chat Global";
 
 ob_start();
 
 ?>
 <link rel="stylesheet" href="assets/CSS/chatGlobal.css">
 
+<p style="display:none" id="user-pseudo"><?= $info['pseudo'] ?></p>
+
 <div id="chat">
     <div id="chatBox"></div>
+
+    <?php
+
+    include_once __DIR__ . '/../../utils/alert_errors.php';
+
+    ?>
     
-    <form id="formSendMessage" method="post" action="/actions/sendMessages.php">
+    <form id="formSendMessage">
         <!-- <label for="sendMessage" id="sendMessagePH">Envoyer un message</label> -->
         <input type="text" id="sendMessage" name="sendMessage">
         <button id="submit">Envoyer</button>
@@ -27,6 +39,7 @@ ob_start();
 <script>
     let autoScrollEnabled = true;
     let lastDataLength = 0;
+    let pseudo = document.getElementById('user-pseudo').innerHTML;
 
     setInterval(() => {
         const xhr = new XMLHttpRequest();
@@ -43,10 +56,20 @@ ob_start();
 
                 // Loop through the data and display it
                 for (let i = 0; i < jsonData.length; i++) {
+
                     let newDiv = document.createElement('div');
-                    newDiv.className = 'messageBox';
-                    newDiv.innerHTML = `<div class="Pseudo">${jsonData[i].pseudo}</div>`;
-                    newDiv.innerHTML += `<div class="message">${jsonData[i].message}</div>`;
+                    
+
+                    if(jsonData[i].pseudo === pseudo) {
+                        newDiv.className = 'you-messageBox';
+                        newDiv.innerHTML = `<div class="you">Vous</div>`;
+                        newDiv.innerHTML += `<div class="you-message">${jsonData[i].message}</div>`;
+                    }
+                    else {
+                        newDiv.className = 'other-messageBox';
+                        newDiv.innerHTML = `<div class="other">${jsonData[i].pseudo}</div>`;
+                        newDiv.innerHTML += `<div class="other-message">${jsonData[i].message}</div>`;
+                    }
                     document.getElementById('chatBox').appendChild(newDiv);
                 }
 
@@ -65,6 +88,24 @@ ob_start();
     document.getElementById("chatBox").addEventListener("click", () => {
     autoScrollEnabled = !autoScrollEnabled;
     });
+
+    let form = document.getElementById('formSendMessage');
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        let message = document.getElementById('sendMessage').value;
+        let user_id = <?= $_SESSION['user_id'] ?>;
+
+        const xhr2 = new XMLHttpRequest();
+        xhr2.open('POST', '/actions/sendMessages.php');
+        xhr2.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr2.send(`message=${encodeURIComponent(message)}&user_id=${encodeURIComponent(user_id)}`);
+
+        document.getElementById('sendMessage').value = '';
+    });
+
+    //let submitButton = document.getElementById('submit');
+
 </script>
 
 <?php
--- www/actions/sendMessages.php
+++ www/actions/sendMessages.php
@@ -1,11 +1,18 @@
 <?php
-
 require_once __DIR__ . "/../../src/init.php";
 
-$messageToSend = htmlspecialchars($_POST['sendMessage']);
+//user message need here to be sent to the database
+if (empty($_POST['message'])) {
+    display_errors("Message vide", '/?page=chatGlobal');
+}
+$messageToDB = htmlspecialchars($_POST['message']);
+$user_id = $_POST['user_id'];
 
+$getUsername = $db->prepare("SELECT pseudo FROM users WHERE user_id=?");
+$getUsername->execute([$user_id]);
+$username = $getUsername->fetch();
 
-
-
-
-header('Location: /?page=home');
+$create_user = $db->prepare("INSERT INTO ChatGlobal(user_id, pseudo, `message`) VALUES(?, ?, ?)");
+$create_user->execute([
+    $user_id, $username[0], $messageToDB
+]);
